7578857ba634c686db71e08db6bec29dbd7e4e41
lostan89 <skeeperenator@gmail.com>
2026-07-01 18:19:10 +0300

    reduce queries count with prefetch_related for author

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,19 @@ def serialize_post(post):
         'first_tag_title': post.tags.all()[0].title,
     }
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
 
 def serialize_tag(tag):
     return {
@@ -32,13 +45,14 @@ def serialize_tag(tag):
 
 def index(request):
 
-    popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')
+    popular_posts = Post.objects.prefetch_related('author').annotate(likes_count=Count('likes')).order_by('-likes_count')
     most_popular_posts = popular_posts[:5]
 
-    fresh_posts = Post.objects.order_by('published_at')
+    fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
     tags = Tag.objects.all()
+    # tags = Tag.objects.prefetch_related('posts')
     # popular_tags = sorted(tags, key=get_related_posts_count)
     popular_tags = tags.annotate(tags_count=Count('posts')).order_by('-tags_count')
     most_popular_tags = popular_tags[:5]
